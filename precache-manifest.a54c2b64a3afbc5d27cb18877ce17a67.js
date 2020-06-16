1536f7177dfd59f2eda2de384f1376148ae1d2a6
Vinicius <vrf92@msn.com>
2020-06-15 22:07:09 -0300

    Updates

---
+++ precache-manifest.a54c2b64a3afbc5d27cb18877ce17a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bda41c1983cb133987759047badb97",
+    "url": "/counter-react.js/index.html"
+  },
+  {
+    "revision": "6b659f0356f03239582d",
+    "url": "/counter-react.js/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "ea63ec3680ff5471cdca",
+    "url": "/counter-react.js/static/css/main.162ceb3a.chunk.css"
+  },
+  {
+    "revision": "6b659f0356f03239582d",
+    "url": "/counter-react.js/static/js/2.77aec319.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-react.js/static/js/2.77aec319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea63ec3680ff5471cdca",
+    "url": "/counter-react.js/static/js/main.a3f44334.chunk.js"
+  },
+  {
+    "revision": "948aa6946fddc93fb725",
+    "url": "/counter-react.js/static/js/runtime-main.ff8249c6.js"
+  }
+]);
